e14f88418e37971f695181ecdfce4716c6fe5119
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-10-06 23:23:01 +0300

    init project

---
+++ gulpfile.mjs
@@ -0,0 +1,140 @@
+import { src, dest, series, parallel, watch, task } from 'gulp';
+
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import gulpMode from 'gulp-mode';
+import sourcemaps from 'gulp-sourcemaps';
+import noop from 'gulp-noop';
+import concat from 'gulp-concat';
+import rename from 'gulp-rename';
+import autoprefixer from 'gulp-autoprefixer';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+import newer from 'gulp-newer';
+import imagemin from 'gulp-imagemin';
+import fonter from 'gulp-fonter';
+import ttfWoff from 'gulp-ttf2woff2';
+import { deleteAsync } from 'del';
+
+const sass = gulpSass(dartSass);
+const mode = gulpMode({
+	modes: ['production', 'development'],
+	defaultMode: 'development',
+	verbose: false,
+});
+
+const isProduction = mode.production();
+const bs = browserSync.create();
+
+function styles() {
+	return src('app/scss/*.scss')
+		.pipe(isProduction ? noop() : sourcemaps.init())
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: [
+					'> 1%',
+					'ie >= 8',
+					'edge >= 15',
+					'ie_mob >= 10',
+					'ff >= 45',
+					'chrome >= 45',
+					'safari >= 7',
+					'opera >= 23',
+					'ios >= 7',
+					'android >= 4',
+					'bb >= 10',
+				],
+				grid: true,
+			}),
+		)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({ outputStyle: isProduction ? 'compressed' : 'expanded' }))
+		.pipe(isProduction ? noop() : sourcemaps.write(''))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(dest('app/css'))
+		.pipe(bs.stream());
+}
+
+function scripts() {
+	return src('app/scripts/index.js')
+		.pipe(concat('index.min.js'))
+		.pipe(mode.production(uglify()))
+		.pipe(dest('app/scripts'))
+		.pipe(bs.stream());
+}
+
+function images() {
+	return src(['app/img/src/*.*', '!app/img/src/*.svg'], { encoding: false })
+		.pipe(newer('app/img/dist'))
+		.pipe(imagemin({ verbose: true }))
+		.pipe(dest('app/img/dist'));
+}
+
+async function fonts() {
+	return src('app/fonts/src/*.*', { encoding: false })
+		.pipe(newer('app/fonts/dist'))
+		.pipe(fonter({ formats: ['woff', 'ttf'] }))
+		.pipe(dest('app/fonts/dist'))
+		.pipe(src('app/fonts/*.ttf'))
+		.pipe(ttfWoff())
+		.pipe(dest('app/fonts/dist'));
+}
+
+function html() {
+	return src('app/*.html').pipe(dest('app')).pipe(bs.stream());
+}
+
+async function cleanDist() {
+	await deleteAsync('dist');
+}
+
+async function build() {
+	return src(
+		[
+			'app/css/*.css',
+			'app/fonts/dist/*.*',
+			'!app/css/*.map',
+			'!app/img/src/*.*',
+			'app/img/dist/*.*',
+			'app/scripts/index.min.js',
+			'app/*.html',
+		],
+		{ base: 'app', allowEmpty: true, encoding: false },
+	).pipe(dest('dist'));
+}
+
+function watchFiles() {
+	if (!isProduction) {
+		bs.init({
+			server: {
+				baseDir: 'app/',
+			},
+			open: true,
+		});
+	}
+	watch(['app/scss/**/*.scss'], styles);
+	watch(['app/scripts/**/*.js', '!app/scripts/index.min.js'], scripts);
+	watch(['app/img/src/'], images);
+	watch(['app/*.html']).on('change', bs.reload);
+}
+
+task('styles', styles);
+task('scripts', scripts);
+task('clean', cleanDist);
+task('images', images);
+task('fonts', fonts);
+task('html', html);
+task('watch', watchFiles);
+task('production', build);
+
+async function allTasks() {
+	return isProduction
+		? series('styles', 'scripts', 'production')()
+		: parallel('html', 'styles', 'images', 'fonts', 'scripts', 'watch')();
+}
+
+// gulp build --production для сборки на production
+task('build', series(cleanDist, allTasks));
+
+// gulp для сборки на develop
+task('default', allTasks);
